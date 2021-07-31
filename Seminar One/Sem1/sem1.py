f4b33f79493d392bffd203898f24ef85117bf192
student <paulwuhj@suss.edu.sg>
2021-07-31 15:02:11 +0800

    31/07/2021-15:02

---
+++ Seminar One/Sem1/sem1.py
@@ -0,0 +1,31 @@
+def main():
+    print("Hello World!")
+
+# a = main()
+
+#print(a)
+
+print(2+3)
+
+score = 5
+name = "Alan"
+
+print(name, "scored", score, "points")
+
+birthyear = 2000
+currentyear = 2021
+print("Age is", currentyear - birthyear)
+print("Age is", 2021 - birthyear)
+
+# This is what the following codes do
+
+name = input("Enter friend's name: ") # This is okay also
+
+ # print("Hello", name)
+
+# score = int(input("Enter score: "))
+score = float(input("Enter score: "))
+score = score + 4
+print(score)
+
+
